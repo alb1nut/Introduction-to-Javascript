679f47ac99452f6a0a4a45e85c6191e6b45563ac
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-23 18:42:54 +0000

    update app.js

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ let myname ='Albert Nutifafa Dovlo';
 let height =182;
 let country ='Ghana'
 
-console.log(`My name is ${myname}. I am ${height} tall. I am from ${country}.`)
+console.log(`My name is ${myname}. I am ${height}cm tall. I am from ${country}.`)
